5b5fc331ac807ff51484fadfd7619e3929b8a91a
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-07-22 17:18:29 +0000

    Create routes for different die

--- dice.rb
+++ dice.rb
@@ -1,10 +1,75 @@
 require "sinatra"
-
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+  <a href='/dice/2/6'>Roll 2 6-sided dice</a>
+  <a href='/dice/2/10'>Roll 2 10-sided dice</a>
+  <a href='/dice/1/20'>Roll 20-sided die</a>
+  <a href='/dice/5/4'>Roll 5 4-sided die</a>"
 end
 
 get("/zebra") do
-  "We must add a route for each path we want to support"
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>Dice Roll</h1>
+    <h2>2d6</h2>
+   <p>#{outcome}</p>
+   <a href='/'>Home</a>
+   <a href='/dice/2/6'>Reroll</a>"
+end
+
+get("/dice/2/10") {
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>Dice Roll</h1>
+    <h2>2d10</h2>
+   <p>#{outcome}</p>
+   <a href='/'>Home</a>
+   <a href='/dice/2/10'>Reroll</a>"
+}
+
+
+
+get("/dice/1/20") {
+  die = rand(1..20)
+  outcome = "You rolled a #{die}."
+
+  "<h1>Dice Roll</h1>
+    <h2>1d20</h2>
+   <p>#{outcome}</p>
+   <a href='/'>Home</a>
+   <a href='/dice/1/20'>Reroll</a>"
+}
+
+
+get("/dice/5/4") {
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>Dice Roll</h1>
+    <h2>5d4</h2>
+   <p>#{outcome}</p>
+   <a href='/'>Home</a>
+   <a href='/dice/5/4'>Reroll</a>"
+}
